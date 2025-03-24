47327e19820854809636c38eabe4284bc7f3b979
prasangatiwari <prasangahere@gmail.com>
2025-03-24 15:25:38 -0400

    updateUser created

--- resources/queries.js
+++ resources/queries.js
@@ -33,6 +33,22 @@ const deleteUser = (request, response) => {
     })
 }
 
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, email } = request.body
+
+    pool.query(
+        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+        [name, email, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified with ID: ${id}`)
+        }
+    )
+}
+
 
 module.exports = {
     getUsers,
