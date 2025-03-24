c6c3a407ea1e365f89d024fd2ba7760b3b4b3f19
prasangatiwari <prasangahere@gmail.com>
2025-03-24 15:26:12 -0400

    getUserById created

--- resources/queries.js
+++ resources/queries.js
@@ -49,6 +49,17 @@ const updateUser = (request, response) => {
     )
 }
 
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
 
 module.exports = {
     getUsers,
